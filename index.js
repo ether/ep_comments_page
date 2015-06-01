1712fca0c468bceda308a7ffc49db7dc14a6a713
Luiza Pagliari <lpagliar@thoughtworks.com>
2015-06-01 15:52:00 -0300

    Providing server-side setting to control if comments will be displayed as icons or boxes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
+var settings = require('ep_etherpad-lite/node/utils/Settings');
 var formidable = require('formidable');
 var clientIO = require('socket.io-client');
 var commentManager = require('./commentManager');
@@ -116,6 +117,11 @@ exports.eejsBlock_scripts = function (hook_name, args, cb) {
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_comments_page/templates/styles.html", {}, module);
   return cb();
+};
+
+exports.clientVars = function (hook, context, cb) {
+  var displayCommentAsIcon = settings.ep_comments_page ? settings.ep_comments_page.displayCommentAsIcon : false;
+  return cb({ "displayCommentAsIcon": displayCommentAsIcon });
 };
 
 exports.expressCreateServer = function (hook_name, args, callback) {
