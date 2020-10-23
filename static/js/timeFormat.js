c33ba71c68c3ac2de393f0e1de24cc7c0a69412f
Richard Hansen <rhansen@rhansen.org>
2020-10-23 02:39:37 -0400

    Use html10n's `plural` macro to properly handle plurals

--- static/js/timeFormat.js
+++ static/js/timeFormat.js
@@ -1,38 +1,15 @@
-const localizable = typeof html10n !== 'undefined';
+/* global exports, html10n */
 
-const l10nKeys = {
-  "seconds"           : "ep_comments_page.time.seconds",
-  "1 minute ago"      : "ep_comments_page.time.one_minute",
-  "minutes"           : "ep_comments_page.time.minutes",
-  "1 hour ago"        : "ep_comments_page.time.one_hour",
-  "hours"             : "ep_comments_page.time.hours",
-  "yesterday"         : "ep_comments_page.time.one_day",
-  "days"              : "ep_comments_page.time.days",
-  "last week"         : "ep_comments_page.time.one_week",
-  "weeks"             : "ep_comments_page.time.weeks",
-  "last month"        : "ep_comments_page.time.one_month",
-  "months"            : "ep_comments_page.time.months",
-  "last year"         : "ep_comments_page.time.one_year",
-  "years"             : "ep_comments_page.time.years",
-  "last century"      : "ep_comments_page.time.one_century",
-  "centuries"         : "ep_comments_page.time.centuries"
-}
+const localizable = typeof html10n !== 'undefined';
 
 const time_formats = [
   [60, 'seconds', 1], // 60
-  [120, '1 minute ago', '1 minute from now'], // 60*2
   [3600, 'minutes', 60], // 60*60, 60
-  [7200, '1 hour ago', '1 hour from now'], // 60*60*2
   [86400, 'hours', 3600], // 60*60*24, 60*60
-  [172800, 'yesterday', 'tomorrow'], // 60*60*24*2
   [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-  [1209600, 'last week', 'next week'], // 60*60*24*7*4*2
   [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-  [4838400, 'last month', 'next month'], // 60*60*24*7*4*2
   [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-  [58060800, 'last year', 'next year'], // 60*60*24*7*4*12*2
   [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-  [5806080000, 'last century', 'next century'], // 60*60*24*7*4*12*100*2
   [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
 ];
 
@@ -48,9 +25,10 @@ function prettyDate(time){
   for (const format of time_formats) {
     if (seconds < format[0]) {
       const count = Math.floor(seconds / format[2]);
-      if (localizable) return html10n.get(l10nKeys[format[1]] + l10n_appendix, {count});
-      if (typeof format[2] === 'string') return format[list_choice];
-      return `${count} ${format[1]} ${token}`;
+      if (localizable) {
+        return html10n.get(`ep_comments_page.time.${format[1]}${l10n_appendix}`, {count});
+      }
+      return `${count} ${count === 1 ? format[1].slice(0, -1) : format[1]} ${token}`;
     }
   }
   return time;
