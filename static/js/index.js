a663ea668182cd0031728ebe7dc32841cf13a2b5
Richard Hansen <rhansen@rhansen.org>
2020-11-11 17:55:20 -0500

    Switch single and double quotes in HTML string

--- static/js/index.js
+++ static/js/index.js
@@ -616,7 +616,7 @@ ep_comments.prototype.insertContainers = function () {
   const target = $('iframe[name="ace_outer"]').contents().find('#outerdocbody');
 
   // Create hover modal
-  target.prepend("<div class='comment-modal popup'><div class='popup-content comment-modal-comment'></div></div>");
+  target.prepend('<div class="comment-modal popup"><div class="popup-content comment-modal-comment"></div></div>');
 
   // Add comments side bar container
   target.prepend('<div id="comments"></div>');
