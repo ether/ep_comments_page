8c326ee9e76d439c883003fb7f32855ad4a5c8c9
Richard Hansen <rhansen@rhansen.org>
2020-11-16 14:05:11 -0500

    Return `no_such_comment` error if comment does not exist
    There is no client-side logic to print a pretty localized error
    message because this should never happen.

--- commentManager.js
+++ commentManager.js
@@ -15,10 +15,8 @@ exports.getComments = async (padId) => {
 };
 
 exports.deleteComment = async (padId, commentId, authorId) => {
-  let comments = await db.get('comments:' + padId);
-  // the entry doesn't exist so far, let's create it
-  if (comments == null) comments = {};
-  // get the entry
+  const comments = await db.get(`comments:${padId}`);
+  if (comments == null || comments[commentId] == null) return 'no_such_comment';
   if (comments[commentId].author !== authorId) {
     return 'unauth';
   }
@@ -183,6 +181,7 @@ exports.changeCommentText = async (padId, commentId, commentText, authorId) => {
 
   // get the entry
   const comments = await db.get(prefix + padId);
+  if (comments == null || comments[commentId] == null) return 'no_such_comment';
   if (comments[commentId].author !== authorId) {
     return 'unauth';
   }
