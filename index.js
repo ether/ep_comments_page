ether/ep_comments_page

commit e32ec365c78237a4c6ca740d8ffe8ee5421e3627

Simplify handleMessageSecurity hook function
I can't wait until we can use the optional chaining operator (node.js
v14 and later).

index.js
@@ -23,21 +23,12 @@ exports.padCopy = async (hookName, context) => {
 };
 
 exports.handleMessageSecurity = function(hook_name, context, callback){
-  if(context.message && context.message.data && context.message.data.apool){
-    var apool = context.message.data.apool;
-    if(apool.numToAttrib && apool.numToAttrib[0] && apool.numToAttrib[0][0]){
-      if(apool.numToAttrib[0][0] === "comment"){
-        // Comment change, allow it to override readonly security model!!
-        return callback(true);
-      }else{
-        return callback();
-      }
-    }else{
-      return callback();
-    }
-  }else{
-    return callback();
+  const {message: {data: {apool} = {}} = {}} = context;
+  if (apool && apool[0] && apool[0][0] === 'comment') {
+    // Comment change, allow it to override readonly security model!!
+    return callback(true);
   }
+  return callback();
 };
 
 exports.socketio = function (hook_name, args, cb){

commit d88abdef810653f6a9049fe883269126725b0559

Fix comment change detection
This fixes a bug introduced in commit
e32ec365c78237a4c6ca740d8ffe8ee5421e3627.

index.js
@@ -27,11 +27,8 @@ exports.padCopy = async (hookName, context) => {
 };
 
 exports.handleMessageSecurity = (hookName, context, callback) => {
-  const {message: {data: {apool} = {}} = {}} = context;
-  if (apool && apool[0] && apool[0][0] === 'comment') {
-    // Comment change, allow it to override readonly security model!!
-    return callback(true);
-  }
+  const {message: {data: {apool: {numToAttrib: {0: [key] = []} = []} = {}} = {}} = {}} = context;
+  if (key === 'comment') return callback(true);
   return callback();
 };
 

static/tests/backend/specs/readOnlyPad.js
@@ -1,0 +1,79 @@
+'use strict';
+
+const AttributePool = require('ep_etherpad-lite/static/js/AttributePool');
+const Changeset = require('ep_etherpad-lite/static/js/Changeset');
+const assert = require('assert').strict;
+const common = require('ep_etherpad-lite/tests/backend/common');
+const padManager = require('ep_etherpad-lite/node/db/PadManager');
+const readOnlyManager = require('ep_etherpad-lite/node/db/ReadOnlyManager');
+const shared = require('../../../js/shared.js');
+
+describe(__filename, function () {
+  let agent;
+  let pad;
+  let padId;
+  let roPadId;
+  let socket;
+
+  const makeUserChanges = (opcode, attribs) => {
+    const oldLen = pad.text().length;
+    assert(oldLen > 0);
+    const apool = new AttributePool();
+    const op = Changeset.newOp(opcode);
+    op.chars = 1;
+    op.attribs = Changeset.makeAttribsString(opcode, attribs, apool);
+    const assem = Changeset.smartOpAssembler();
+    assem.append(op);
+    const cs = assem.toString();
+    const newLen = oldLen + assem.getLengthChange();
+    const changeset = Changeset.pack(oldLen, newLen, cs, opcode === '+' ? 'x' : '');
+    return {baseRev: pad.head, changeset, apool: apool.toJsonable()};
+  };
+
+  before(async function () {
+    agent = await common.init();
+  });
+
+  beforeEach(async function () {
+    padId = `testpad${common.randomString()}`;
+    assert(!await padManager.doesPadExist(padId));
+    pad = await padManager.getPad(padId, 'text');
+    assert(pad.text().startsWith('text'));
+    roPadId = await readOnlyManager.getReadOnlyId(padId);
+    const res = await agent.get(`/p/${roPadId}`).expect(200);
+    socket = await common.connect(res);
+    const {type, data: clientVars} = await common.handshake(socket, roPadId);
+    assert.equal(type, 'CLIENT_VARS');
+    assert(clientVars.readonly);
+    assert.equal(clientVars.readOnlyId, roPadId);
+  });
+
+  afterEach(async function () {
+    if (socket != null) socket.close();
+    socket = null;
+    if (pad != null) await pad.remove();
+    pad = null;
+  });
+
+  describe('comment-only changes are accepted', function () {
+    it('add/change comment attribute', async function () {
+      await Promise.all([
+        common.waitForAcceptCommit(socket, pad.head + 1),
+        common.sendUserChanges(
+            socket, makeUserChanges('=', [['comment', shared.generateCommentId()]])),
+      ]);
+    });
+
+    it('remove comment attribute', async function () {
+      await Promise.all([
+        common.waitForAcceptCommit(socket, pad.head + 1),
+        common.sendUserChanges(
+            socket, makeUserChanges('=', [['comment', shared.generateCommentId()]])),
+      ]);
+      await Promise.all([
+        common.waitForAcceptCommit(socket, pad.head + 1),
+        common.sendUserChanges(socket, makeUserChanges('=', [['comment', '']])),
+      ]);
+    });
+  });
+});