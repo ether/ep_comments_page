3408aa94a1ef4a4d3ce6c24c2a17949bfab46d00
Richard Hansen <rhansen@rhansen.org>
2022-03-21 19:47:04 -0400

    Let the browser handle escaping

--- static/js/index.js
+++ static/js/index.js
@@ -589,12 +589,10 @@ EpComments.prototype.insertComment = function (commentId, comment, index) {
 
   comment.commentId = commentId;
   comment.reply = true;
-  content = $('#commentsTemplate').tmpl({
-    ...comment,
-    changeArgs: JSON.stringify({
-      changeFrom: comment.changeFrom,
-      changeTo: comment.changeTo,
-    }).replace(/"/g, '&quot;'),
+  content = $('#commentsTemplate').tmpl(comment);
+  content.find('.from-label')[0].dataset.l10nArgs = JSON.stringify({
+    changeFrom: comment.changeFrom,
+    changeTo: comment.changeTo,
   });
   if (comment.author !== clientVars.userId) {
     $(content).find('.comment-actions-wrapper').addClass('hidden');
